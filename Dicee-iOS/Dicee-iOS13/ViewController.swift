34d0d4fb81a30f3f129a2ea43b327fdfa3c7187d
Vivek Patel <vivekpatel@viveks-air.home>
2023-05-11 15:18:57 -0400

    check

---
+++ Dicee-iOS/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var leftSideDiceImage: UIImageView!
+    @IBOutlet weak var rightSideDiceImage: UIImageView!
+    
+    let diceArray = [UIImage(named: "DiceOne")!,UIImage(named: "DiceTwo")!,UIImage(named: "DiceThree")!,UIImage(named: "DiceFour")!,UIImage(named: "DiceFive")!,UIImage(named: "DiceSix")!]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func scrollBtnTouchUpInside(_ sender: Any) {
+        leftSideDiceImage.image = diceArray.randomElement()
+        rightSideDiceImage.image = diceArray.randomElement()
+        
+    }
+    
+}
+
