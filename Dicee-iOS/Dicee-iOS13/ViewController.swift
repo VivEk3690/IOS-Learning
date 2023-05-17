3accd37bd72271cae39eac649be94155ee0ace84
Vivek Patel <vivekpatel@viveks-air.home>
2023-05-17 16:11:03 -0400

    update constrain

--- Dicee-iOS/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS/Dicee-iOS13/ViewController.swift
@@ -13,7 +13,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var leftSideDiceImage: UIImageView!
     @IBOutlet weak var rightSideDiceImage: UIImageView!
-    
+  
+    @IBOutlet weak var logo: UIImageView!
+ 
     let diceArray = [UIImage(named: "DiceOne")!,UIImage(named: "DiceTwo")!,UIImage(named: "DiceThree")!,UIImage(named: "DiceFour")!,UIImage(named: "DiceFive")!,UIImage(named: "DiceSix")!]
     
     override func viewDidLoad() {
@@ -22,10 +24,15 @@ class ViewController: UIViewController {
     }
 
     
+    
+    
     @IBAction func scrollBtnTouchUpInside(_ sender: Any) {
         leftSideDiceImage.image = diceArray.randomElement()
         rightSideDiceImage.image = diceArray.randomElement()
         
+    }
+    @IBAction func nextPageTouchUpInside(_ sender: Any) {
+    
     }
     
 }
