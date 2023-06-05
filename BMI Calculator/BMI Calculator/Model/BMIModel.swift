039590bfb534f0d8d28ffb1d5a9ccf84c2674056
VivEk3690 <viveksp369@gmail.com>
2023-06-05 16:06:09 -0400

    add BMiCalculator

---
+++ BMI Calculator/BMI Calculator/Model/BMIModel.swift
@@ -0,0 +1,14 @@
+//
+//  BMIModel.swift
+//  BMI Calculator
+//
+//  Created by Vivek Patel on 2023-06-03.
+//
+
+import UIKit
+
+struct BMIModel{
+    var bmi:String?
+    var advice:String?
+    var color:UIColor?
+}
